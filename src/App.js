63b9a4c792cb8ef785ac12dc373fbd0dafc5ff81
odadki <93558662+odadki@users.noreply.github.com>
2022-01-16 16:55:50 -0800

    Added open-sourced link to App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,22 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather app</h1>
+      <footer>
+        This project is{" "}
+        <a
+          href="https://github.com/odadki/react-weather-app"
+          target="blank"
+          rel="noreferrer"
+        >
+          open-sourced
+        </a>{" "}
+        and coded by Danielle Oda
+      </footer>
+    </div>
+  );
 }
 
 export default App;
