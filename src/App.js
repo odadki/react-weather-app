ba9e84ae2397e18fa56064a8b942694f00503991
odadki <93558662+odadki@users.noreply.github.com>
2022-01-19 12:24:22 -0800

    Added CSS

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
+import Weather from "./Weather";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <h1>Weather app</h1>
+      <Weather />
       <footer>
         This project is{" "}
         <a
